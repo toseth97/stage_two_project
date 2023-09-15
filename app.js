3a75f4d82e3d0367f39f398f4861c9f42c0dc1d1
toseth97 <oluwasheun9721@gmail.com>
2023-09-15 15:29:59 +0100

    change of name

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-// Import required modules
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-// Create an Express application
 const app = express();
+const PORT = process.env.PORT || 3880;
 
 // Set up MongoDB connection (Make sure you have MongoDB running)
 connectDB().catch((err) => console.log(err.message));
@@ -21,93 +20,94 @@ async function connectDB() {
     db.on("error", console.error.bind(console, "MongoDB connection error:"));
 }
 
-// Define a Person schema and model
 const personSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
+    name: String,
     age: Number,
+    email: String,
 });
 
 const Person = mongoose.model("Person", personSchema);
 
-// Middleware for parsing JSON request bodies
 app.use(bodyParser.json());
 
-// CRUD operations
+// Middleware for validating the name field
+function validateName(req, res, next) {
+    const name = req.params.name;
+    if (typeof name !== "string") {
+        return res.status(400).json({ error: "Name should be a string" });
+    }
+    // If validation passes, continue to the next middleware or route handler
+    next();
+}
 
-// CREATE: Adding a new person
-app.post("/api", async (req, res) => {
+// Create a new person with a dynamic name parameter
+app.post("/api/:name", validateName, async (req, res) => {
     try {
-        const { firstName, lastName, age } = req.body;
-        console.log(req.body);
-        const person = new Person({ firstName, lastName, age });
-        const savedPerson = await person.save();
-        res.json(savedPerson);
+        const name = req.params.name;
+        const person = new Person({ name, ...req.body });
+        await person.save();
+        res.json(person);
     } catch (error) {
-        res.status(500).json({ error: "Could not create the person." });
+        res.status(500).json({ error: "An error occurred" });
     }
 });
 
-//READ: Fetching all users
-
+// Get all persons
 app.get("/api", async (req, res) => {
     try {
-        const people = await Person.find();
-        if (people.length < 1) {
-            return res.status(404).json({ error: "Userlist is empty" });
-        }
-        res.json(people);
-    } catch (err) {
-        res.json({ error: err.message });
+        const persons = await Person.find();
+        res.json(persons);
+    } catch (error) {
+        res.status(500).json({ error: "An error occurred" });
     }
 });
 
-// READ: Fetching details of a person by ID
-app.get("/api/:id", async (req, res) => {
+// Get a person by name
+app.get("/api/:name", validateName, async (req, res) => {
     try {
-        const person = await Person.findById(req.params.id);
+        const name = req.params.name;
+        const person = await Person.findOne({ name });
         if (!person) {
-            return res.status(404).json({ error: "Person not found." });
+            return res.status(404).json({ error: "Person not found" });
         }
         res.json(person);
     } catch (error) {
-        res.status(500).json({ error: "Could not fetch the person." });
+        res.status(500).json({ error: "An error occurred" });
     }
 });
 
-// UPDATE: Modifying details of an existing person by ID
-app.put("/api/:id", async (req, res) => {
+// Update a person by name
+app.put("/api/:name", validateName, async (req, res) => {
     try {
-        const { firstName, lastName, age } = req.body;
-        const updatedPerson = await Person.findByIdAndUpdate(
-            req.params.id,
-            { firstName, lastName, age },
-            { new: true } // Return the updated document
+        const name = req.params.name;
+        const updatedPerson = await Person.findOneAndUpdate(
+            { name },
+            req.body,
+            { new: true }
         );
         if (!updatedPerson) {
-            return res.status(404).json({ error: "Person not found." });
+            return res.status(404).json({ error: "Person not found" });
         }
         res.json(updatedPerson);
     } catch (error) {
-        res.status(500).json({ error: "Could not update the person." });
+        res.status(500).json({ error: "An error occurred" });
     }
 });
 
-// DELETE: Removing a person by ID
-app.delete("/api/:id", async (req, res) => {
+// Delete a person by name
+app.delete("/api/:name", validateName, async (req, res) => {
     try {
-        const deletedPerson = await Person.findByIdAndRemove(req.params.id);
+        const name = req.params.name;
+        const deletedPerson = await Person.findOneAndDelete({ name });
         if (!deletedPerson) {
-            return res.status(404).json({ error: "Person not found." });
+            return res.status(404).json({ error: "Person not found" });
         }
-        res.json({ message: "Person deleted successfully." });
+        res.json({ message: "Person deleted" });
     } catch (error) {
-        res.status(500).json({ error: "Could not delete the person." });
+        res.status(500).json({ error: "An error occurred" });
     }
 });
 
-// Start the Express server
-const port = process.env.PORT || 3880;
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
